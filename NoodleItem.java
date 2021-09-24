75754829fe1b4b7887d319165967888e760b06c2
ashlesh9999 <90056040+ashlesh9999@users.noreply.github.com>
2021-09-24 11:03:07 -0500

    Create NoodleItem.java

---
+++ NoodleItem.java
@@ -0,0 +1,23 @@
+public class NoodleItem {
+	String name;
+	String description;
+	double price;
+	
+	public NoodleItem(String name, String description, double price ) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public String toString() {
+		return (name + ", " + description +", " + price);
+	}
+}
